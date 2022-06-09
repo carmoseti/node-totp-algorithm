5deb41ec1ba88309be5a9f89881b855cf3fd2679
Carlton Moseti <carmoseti@gmail.com>
2022-06-09 20:34:01 +0300

    feat: Server totp generation
    - Compatible with Google and Microsoft Authenticator

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,19 @@
-import * as crypto from "crypto"
-import {Algorithms, DigitsCount, Period} from "./config/types"
+import {authenticator} from 'otplib'
 import {base32Encode} from "./util"
+import {Algorithms, DigitsCount, Period} from "./config/types"
 import QRCode from 'qrcode'
 
-const hmac = (algorithm: string, key: string, text: string): Buffer => {
-    return crypto.createHmac(algorithm, Buffer.from(key)).update(Buffer.from(text, 'hex')).digest()
-}
+const generate = (secret: string, period: Period) => {
+    const toDate: Date = new Date()
+    const timeSeconds: number = toDate.getUTCSeconds()
+    const timeElapsed: number = timeSeconds % Number(period)
+    const timeRemaining: number = Number(period) - timeElapsed
 
-const digitsPower: number[] = [
-    1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000
-]
+    console.log(`${authenticator.generate(secret)} => ${toDate.toUTCString()}`)
 
-const generateOTP = (key: string, hexTime: string, returnDigits: number, algorithm: string): string => {
-    let result: string = null
-
-    while (hexTime.length < 16) {
-        hexTime = "0" + hexTime
-    }
-
-    const hashBuffer: Buffer = hmac(algorithm, key, hexTime)
-    // tslint:disable-next-line:no-bitwise
-    const offset: number = hashBuffer[hashBuffer.length - 1] & 0xf
-    // tslint:disable-next-line:no-bitwise
-    const binary: number = ((hashBuffer[offset] & 0x7f) << 24) | ((hashBuffer[offset + 1] & 0xff) << 16) | ((hashBuffer[offset + 2] & 0xff) << 8) | (hashBuffer[offset + 3] & 0xff)
-
-    const otp: number = binary % digitsPower[returnDigits]
-    result = String(otp)
-
-    while (result.length < returnDigits) {
-        result = "0" + result
-    }
-
-    return result
+    setTimeout(() => {
+        generate(secret, period)
+    }, timeRemaining * 1000)
 }
 
 const program = () => {
@@ -39,8 +21,7 @@ const program = () => {
     const type: string = 'totp'
     const app: string = encodeURI('Node Test')
     const accountName: string = 'carmosdevelopers@gmail.com'
-    const secret: string = base32Encode('SECRET@123', 'RFC3548', {padding: false})
-    // TODO:
+    const secret: string = base32Encode('RANDOM_SECRET@1234567890', 'RFC4648', {padding: false})
     const algorithm: Algorithms = 'SHA1'
     const digitsCount: DigitsCount = '6'
     const period: Period = '30'
@@ -48,26 +29,14 @@ const program = () => {
 
     const keyUri: string = `otpauth://${type}/${app}:${accountName}?${query}`
 
-    /*// Generate QR code of keyUri and display
+    // Generate QR code of keyUri and display
     QRCode.toString(keyUri, {type: "terminal"}, (error: Error, url: string) => {
         if (error)
             throw error
-
         console.log(url)
-    })*/
+    })
 
-    const toDate: Date = new Date()
-    toDate.setHours(toDate.getUTCHours())
-    const T0: number = 0
-    const X: number = 30
-    let steps: string = "0"
-    const time: number = Math.floor(toDate.getTime() / 1000)
-    const T: number = Math.floor((time - T0) / X)
-    steps = T.toString(16).toUpperCase()
-
-    console.log(steps)
-    // TODO:
-    console.log(generateOTP(secret, steps, Number(digitsCount), 'sha1'))
+    generate(secret, period)
 }
 
 program()
--- src/util/index.ts
+++ src/util/index.ts
@@ -59,3 +59,62 @@ export const base32Encode = (data: string, variant: Base32Variant, options ?: Ba
 
     return output
 }
+
+const readCharacter = (alphabet: string, character: string): number => {
+    const idx: number = alphabet.indexOf(character)
+
+    if (idx === -1) {
+        throw new Error('Invalid character found: ' + character)
+    }
+
+    return idx
+}
+
+export const base32Decode = (input: string, variant: Base32Variant): ArrayBuffer => {
+    let alphabet: string
+
+    switch (variant) {
+        case 'RFC3548':
+        case 'RFC4648':
+            alphabet = RFC4648
+            input = input.replace(/=+$/, '')
+            break
+        case 'RFC4648-HEX':
+            alphabet = RFC4648_HEX
+            input = input.replace(/=+$/, '')
+            break
+        case 'Crockford':
+            alphabet = CROCKFORD
+            input = input.toUpperCase().replace(/O/g, '0').replace(/[IL]/g, '1')
+            break
+        default:
+            throw new Error('Unknown base32 variant: ' + variant)
+    }
+
+    const length: number = input.length
+
+    let bits: number = 0
+    let value: number = 0
+
+    let index: number = 0
+    // tslint:disable-next-line:no-bitwise
+    const output: Uint8Array = new Uint8Array((length * 5 / 8) | 0)
+
+    for (let i: number = 0; i < length; i++) {
+        // tslint:disable-next-line:no-bitwise
+        value = (value << 5) | readCharacter(alphabet, input[i])
+        bits += 5
+
+        if (bits >= 8) {
+            // tslint:disable-next-line:no-bitwise
+            output[index++] = (value >>> (bits - 8)) & 255
+            bits -= 8
+        }
+    }
+
+    return output.buffer
+}
+
+export const stringToHex = (value :string) :string => {
+    return Buffer.from(value,'utf8').toString('hex')
+}
